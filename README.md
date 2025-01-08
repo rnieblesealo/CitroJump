97b08b917547d9b3d0540fc5b76e79f790a1a300
Rafael Niebles <rnieblesealo@gmail.com>
2025-01-08 14:54:15 -0500

    camera

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-TBD!
+### How to do platforms
+- Player "pushes" viewport up if they hit their head against screen top
+- Everytime the player does this, generate some more platforms above him
+- We need to detect when the player is pushing up the viewport
--- Source/GameScene.swift
+++ Source/GameScene.swift
@@ -6,13 +6,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var playerXScale: CGFloat = 1 // We will flip this value so we need to cache the original one
     
     let platform = SKSpriteNode(color: UIColor(cgColor: CGColor(gray: 1, alpha: 1)), size: CGSize(width: 10000, height: 10)) // Temporary invis. platform to test jumping
-   
+  
+    let cam = SKCameraNode()
+    
     let jumpSound = SKAudioNode(fileNamed: "jump.wav")
     let landSound = SKAudioNode(fileNamed: "land.wav")
 
     let motionManager = CMMotionManager()
     let xTiltSensitivity: CGFloat = 500
     let yTiltSensitivity: CGFloat = 0
+  
+    func configureCamera(){
+        // Bind camera node to scene
+        self.camera = cam
+        self.addChild(cam) // MARK: Why does it make sense to child it?
+    }
     
     func configureMotion(){
         // Start accelerometer
@@ -107,9 +115,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         jumpSound.run(SKAction.play())
     }
+   
+    func createPlatform(at position: CGPoint) -> SKSpriteNode {
+        // Platforms should all have same width, height
+        let platformWidth = player.size.width + (1/2 * player.size.width) // Player's width + 1/4 that width on both sides (so add 1/2 width of player)
+        let platformHeight = player.size.height * (1/10) // Tenth of player's height
+
+        let platform = SKSpriteNode(
+            color: UIColor(cgColor: CGColor(gray: 1, alpha: 1)),
+            size: CGSize(width: platformWidth, height: platformHeight)
+        )
+        
+        // Leave normal anchorpoint (center)
+        
+        return platform
+    }
     
     // Run when moving to scene
     override func didMove(to view: SKView) {
+        configureCamera()
         configureMotion()
         configureSounds()
         configureScenePhysics() // Physics environment only, not individual physics bodies!
@@ -158,6 +182,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     // Run every frame
     override func update(_ currentTime: TimeInterval) {
+        // Keep cam on player
+        
+        // Ensure camera moves up only if the player has passed its current position (doodle jump effect!)
+        if player.position.y > cam.position.y {
+            cam.position.y = player.position.y
+        }
+      
+        // Start with 2 contiguous screen heights
+        // Everytime the player passes a screen height worth of level, instantiate 1 more screen height
+        // Instantiate platforms within this screen height
+        
         // Apply tilt to player body
         if let playerBody = player.physicsBody {
             // Get accelerometer data
